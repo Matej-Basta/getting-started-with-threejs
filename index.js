99a4115d0e1ab8ca0c0ea20f36e7e62817341be3
matej <matej.basta@email.cz>
2024-06-13 18:35:33 +0200

    simple cube + WebGL support check

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import WebGL from "jsm/capabilities/WebGL.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -12,7 +13,7 @@ const aspect = w / h;
 const near = 0.1;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 5;
 
 const scene = new THREE.Scene();
 
@@ -27,7 +28,7 @@ const mat = new THREE.MeshStandardMaterial({
     flatShading: true,
 })
 const mesh = new THREE.Mesh(geo, mat);
-scene.add(mesh);
+//scene.add(mesh);
 
 const wireMat = new THREE.MeshBasicMaterial({
     color: 0xffffff,
@@ -37,14 +38,27 @@ const wireMesh = new THREE.Mesh(geo, wireMat);
 wireMesh.scale.setScalar(1.001);
 mesh.add(wireMesh);
 
+// Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
 const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
 scene.add(hemiLight);
 
 const animate = (t = 0) => {
     requestAnimationFrame(animate);
     mesh.rotation.y = t * 0.0001;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
     controls.update();
 }
 
-animate();
+if (WebGL.isWebGLAvailable()) {
+    animate();
+} else {
+    const warning = WebGL.getWebGLErrorMessage();
+    document.body.appendChild(warning);
+}
