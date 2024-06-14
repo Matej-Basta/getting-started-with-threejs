495bfe7122327d432d02c3a319c6c8fc64548c8e
matej <matej.basta@email.cz>
2024-06-14 13:46:03 +0200

    creating simple lines

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
 import WebGL from "jsm/capabilities/WebGL.js";
-import { createCube, create3DObject } from "./objects.js";
+import { createCube, create3DObject, createLines } from "./objects.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -9,12 +9,13 @@ const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-const fov = 75;
+const fov = 45;
 const aspect = w / h;
-const near = 0.1;
-const far = 10;
+const near = 1;
+const far = 500;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 5;
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
 
@@ -30,7 +31,9 @@ controls.dampingFactor = 0.03;
 
 // Light
 const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
-scene.add(hemiLight);
+
+// Lines
+// const lines = createLines();
 
 const animate = (t = 0) => {
     requestAnimationFrame(animate);
--- objects.js
+++ objects.js
@@ -24,3 +24,14 @@ export const create3DObject = () => {
     mesh.add(wireMesh);
     return mesh;
 }
+
+export const createLines = () => {
+    const material = new THREE.LineBasicMaterial({color: 0x0000ff});
+    const points = [];
+    points.push(new THREE.Vector3(-10, 0, 0));
+    points.push(new THREE.Vector3(0, 10, 0));
+    points.push(new THREE.Vector3(10, 0, 0));
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+    return line;
+}
